2bcf9827bc4f58bf67e54da68fa3d6977d7aabb9
kubrausluca <kubra.usluca.98@gmail.com>
2021-03-28 00:19:58 +0300

    YPageActivity edited

--- app/src/main/java/com/example/activitylifecycle/YPageActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/YPageActivity.kt
@@ -10,11 +10,19 @@ class YPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_y_page)
 
+        /*
         goMainPageButton.setOnClickListener {
             // Anasayfaya donmedik, yeniden açtık -> istenilen bir durum degil
             val intent = Intent(this@YPageActivity, MainActivity::class.java)
             startActivity(intent)
         }
-
+        */
     }
+
+    override fun onBackPressed() {
+        // geri tusu ile direkt anasayfaya donuyoruz
+        val homeIntent = Intent(this@YPageActivity, MainActivity::class.java)
+        startActivity(homeIntent)
+    }
+
 }
